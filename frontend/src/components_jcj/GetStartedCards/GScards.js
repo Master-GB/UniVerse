fcf294d8d5c98242205891040054796437c5ac3e
jeyixel <jjayawardene610@gmail.com>
2025-08-15 16:39:59 +0530

    Implement code changes to enhance functionality and improve performance (twa icons tika dann tyenw)

--- frontend/src/components_jcj/GetStartedCards/GScards.js
+++ frontend/src/components_jcj/GetStartedCards/GScards.js
@@ -1,20 +1,63 @@
-import React from 'react'
-import './gsCards.css' // Assuming you have a CSS file for styling
+import React from "react";
+import "./gsCards.css"; // Assuming you have a CSS file for styling
+import GSusericon from "./icons_gs/add-user.png"; // Example icon import
 
 export default function GScards() {
   return (
-    <div className="gs-cards-root">
+    <div className="gs-cards-spacing">
+      <div className="gs-cards-root">
         {/* card 1 */}
         <div className="gs-cards__container">
-            <div className="gs-cards__box">
-                <span className="gs-cards__title">Academic and exam prep</span>
-                <div>
-                <strong>JOE WATSON SBF</strong>
-                <p>0000 000 000 0000</p>
-                <span>VALID</span> <span>01/28</span>
-                </div>
+          <div className="gs-cards__box">
+            {/* Replace with actual image or illustration */}
+            <div className="gs-cards__media">
+              <img src={GSusericon} alt="Resource Hub" />
             </div>
+
+            <div className="gs-cards__footer">
+              <span className="gs-cards__title">Resource Hub</span>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div className="gs-cards-root">
+        {/* card 2 */}
+        <div className="gs-cards__container">
+          <div className="gs-cards__box">
+            <div className="gs-cards__media">Image/illustration</div>
+            <div className="gs-cards__footer">
+              <span className="gs-cards__title">Career Guidance</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="gs-cards-root">
+        {/* card 3 */}
+        <div className="gs-cards__container">
+          <div className="gs-cards__box">
+            <div className="gs-cards__media">Image/illustration</div>
+            <div className="gs-cards__footer">
+              <span className="gs-cards__title">
+                Academic and exam preparation
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="gs-cards-root">
+        {/* card 4 */}
+        <div className="gs-cards__container">
+          <div className="gs-cards__box">
+            <div className="gs-cards__media">Image/illustration</div>
+            <div className="gs-cards__footer">
+              <span className="gs-cards__title">Skill development</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
