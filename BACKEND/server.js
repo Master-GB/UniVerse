830bb842d286967c724de7911999209a174e4297
Master-GB <mastergba2001@gmail.com>
2025-08-15 17:52:30 +0530

    Student request Guidance backend completed

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -6,11 +6,15 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const app = express();
 
+const guidanceRouter = require("./routes/student_routes/guidanceR");
+
 const PORT = process.env.PORT || 8070;
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+
+app.use("/guidance",guidanceRouter);
 
 const URL = process.env.MONGODB_URL;
 mongoose.connect(URL);
