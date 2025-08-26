80bea32b80c11e654af30de5aee7b8ffc5fe1645
jeyixel <jjayawardene610@gmail.com>
2025-08-26 15:04:43 +0530

    JCJ's app.js, expect some merging conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,9 @@ import StuDashboard from "./components_stu/stu_dashboard/stu_dashboard";
 import StuGuidance from "./components_stu/stu_guidance/stu_guidance";
 import StuResources from "./components_stu/stu_resources/stu_resources";
 import LandingPage from "./components_jcj/LandingPage_jcj/LandingPage_jcj";
-import MentorDashboard  from "./mentor-components/mentor-dashboard/MentorDashboard";
+import AboutUs_jcj from "./components_jcj/Aboutus_jcj/AboutUs_jcj";
+import SupportPage_jcj from "./components_jcj/SupportPage_jcj/SupportPage_jcj";
+import MentorDashboard from "./mentor-components/mentor-dashboard/MentorDashboard";
 import MentorSessionCreate from "./mentor-components/mentor-session-create/MentorSessionCreate";
 
 const StudentLayout = () => (
@@ -39,7 +41,23 @@ const HomeLayout = () => (
     <React.Fragment>
       <Routes>
         <Route path="/mentor-dashboard" element={<MentorDashboard />} />
-        <Route path="/mentor-session-create" element={<MentorSessionCreate />} />
+        <Route
+          path="/mentor-session-create"
+          element={<MentorSessionCreate />}
+        />
+      </Routes>
+    </React.Fragment>
+  </div>
+);
+
+// jcj
+const LandingPageLayout = () => (
+  <div className="App">
+    <React.Fragment>
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path="about" element={<AboutUs_jcj />} />
+        <Route path="support" element={<SupportPage_jcj />} />
       </Routes>
     </React.Fragment>
   </div>
@@ -49,9 +67,14 @@ function App() {
   return (
     <div className="app-container">
       <Routes>
-         <Route path="/*" element={<StudentLayout />} />
-         <Route path="/hj*" element={<HomeLayout />} />
-         <Route path="/jj*" element={<MentorLayout />} />
+        {/* Mewa main ekt dannepa unless mapping krnw nm */}
+        <Route path="/*" element={<LandingPageLayout />} />
+
+        {/* Student pages */}
+        <Route path="/student/*" element={<StudentLayout />} />
+
+        {/* Mentor pages */}
+        <Route path="/mentor/*" element={<MentorLayout />} />
       </Routes>
     </div>
   );
